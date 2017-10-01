6bfa224b1f3c8811ef1bbd49bb82d6ca918dcb60
Lorenzo Fundaro <lorenzo.fundaro@goeuro.com>
2017-10-01 04:07:55 +0200

    Improve cheers descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ require 'factory_girl'
 
 c = FactoryGirl.create(:campaign)
 FactoryGirl.create(:beer, campaign: c)
-FactoryGirl.create(:cheer, title: "Thank you!", description: "ashf ahsdkljfa ljksdhf kljahsdklfa kljsdhf fasjhd ksjahf", campaign: c, value_cents: 5000, value_currency: "EUR")
-FactoryGirl.create(:cheer, title: "You love beer!", description: "ashf ahsdkljfa ljksdhf kljahsdklfa kljsdhf fasjhd ksjahf", campaign: c, value_cents: 10000, value_currency: "EUR")
-FactoryGirl.create(:cheer, title: "Beer master!", description: "ashf ahsdkljfa ljksdhf kljahsdklfa kljsdhf fasjhd ksjahf", campaign: c, value_cents: 20000, value_currency: "EUR")
+FactoryGirl.create(:cheer, title: "Thank you!", description: "We give you two crates", campaign: c, value_cents: 5000, value_currency: "EUR")
+FactoryGirl.create(:cheer, title: "You love beer!", description: "We give you four crates and a tour", campaign: c, value_cents: 10000, value_currency: "EUR")
+FactoryGirl.create(:cheer, title: "Beer master!", description: "We give you a crater per month for two years", campaign: c, value_cents: 20000, value_currency: "EUR")
